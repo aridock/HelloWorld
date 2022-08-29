c78361bb28fce3df36bbe4c48b39776a84007cf5
aridock <100530580+aridock@users.noreply.github.com>
2022-08-29 12:50:58 -0600

    Pseudocode practice

--- main.ts
+++ main.ts
@@ -1,4 +1,31 @@
-basic.showString("Hello world!")
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . # # # .
+        # # # # #
+        # . # . #
+        # # # # #
+        # . # . #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . # # # .
+        # # # # #
+        # # # # #
+        # # # # #
+        . # . # .
+        `)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showString("Ouch!")
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . # . .
+        . # # # .
+        . # . # .
+        `)
+})
 basic.forever(function () {
 	
 })
